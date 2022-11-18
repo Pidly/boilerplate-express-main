b094e64edf857a8f5819ad67fdabbddfcb5c1e7e
unknown <ji773rs@gmail.com>
2022-11-17 22:43:12 -0800

    added logging for req info

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ var publicPath = path.join(__dirname, "/public");
 
 app.use("/public", express.static(publicPath));
 
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+})
+
 app.get('/json', (req, res) => {
     if(process.env.MESSAGE_STYLE === "uppercase") {
         res.send({"message": "HELLO JSON"});
