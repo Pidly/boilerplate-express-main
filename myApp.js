e221494f73a4b152cc57673db0711cc075acdb0a
unknown <ji773rs@gmail.com>
2022-11-17 17:46:07 -0800

    Added a get method to return some json

--- myApp.js
+++ myApp.js
@@ -5,6 +5,11 @@ var path = require('path');
 var publicPath = path.join(__dirname, "/public");
 
 app.use("/public", express.static(publicPath));
+
+app.get('/json', (req, res) => {
+    res.send({"message": "Hello json"});
+})
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
